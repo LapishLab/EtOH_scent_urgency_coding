66e16a0c377ae4c8b555022da44562f9e7cf11cc
arremes <arremes417@gmail.com>
2025-12-22 11:59:10 -0500

    combined ratsInfo table to contain both wistars and P rats

--- ratInformationTables/ratByRatInfo.m
+++ ratInformationTables/ratByRatInfo.m
@@ -1,53 +1,34 @@
-function [ratsInfo] = ratByRatInfo(IAP_pat, ratSheet)
+function [ratsInfo_all] = ratByRatInfo(IAP_pat, ratSheet)
 %% Organize rat by rat Identifying Information using the IAP Sheet
-% Organizes a bunch of information about each rat so that I can use it for
+% Contains information about each rat so that I can use it for
 % indexing and organizing graphs and data later. 
-% input: path to excel file with rat information, specific sheet in this
-% file, RAP and IAP Cons tables already made
+% input: path to excel file with rat information (can contain multiple), specific sheet in this
+% file 
 % ouptut: a variable that contains columns of classifying information for
 % each rat row 
 
-%Read the rat by rat individual data from the IAP excel sheet into a table 
-ratsInfo = readtable(IAP_pat, 'Sheet', ratSheet);
+%import the rat by rat information for the first cohort
+%ratsInfo_all = readtable(IAP_pat(1), 'Sheet', ratSheet);
+%remove any excess notes under the information table that doesn't align
+%with the information in the column
+%[max_ratID, max_index] = max(ratsInfo_all.ratID);
+%ratsInfo_all([max_index+1:end], :) = [];
 
-%rename the ratID title
-ratsInfo.Properties.VariableNames(1) = "ratID";
-
-%use function to remove the rats that didn't make it 
-%ratsInfo = removeRats(ratsInfo);
-
-%Create the group numbers for the CS+ and CS- scents
-%Group1: got the CS+ on Wednesday in week 1 and Friday in Week 2
-%Group2: got the CS+ on Friday in week 1 and Wednesday in Week 2
-%scentWksGrp = (string(ratsInfo.W1_CS_Access) == "Wednesday");
-%ratsInfo.scentTrtmGrp(scentWksGrp) = 1;
-%ratsInfo.scentTrtmGrp(~scentWksGrp) = 2;
-
-%switch the CPPish Scent Side Assignment to show the side for the CS+
-%rather than the CS- 
-%CSMSide = string(ratsInfo.CPP_CS_SideAssignment) == "Left";
-%ratsInfo.CPPishCSPlSide(CSMSide) = "R";
-%ratsInfo.CPPishCSPlSide(~CSMSide) = "L";
-
-%Add a column for Control and EtOH Clarification
-%ratsInfo.Properties.VariableNames("Treatment") = "Trtm";
-%ratsInfo.Trtm([1:20]) = "H2O";
-%ratsInfo.Trtm([21:end]) = "EtOH";
+ratsInfo_all = []
+%import rat by rat information for the following cohorts, matching and
+%combining the tables into one by shared table column headers
+for i = 1:size(IAP_pat, 1) 
+    ratsInfo_next = readtable(IAP_pat(i), 'Sheet', ratSheet);
+    %remove any excess notes under the information table that doesn't align
+    %with the information in the column
+    [max_ratID, max_index] = max(ratsInfo_next.ratID);
+    ratsInfo_next([max_index+1:end], :) = [];
+    %combine tables together
+    if i > 1;
+        ratsInfo_all = outerjoin(ratsInfo_all, ratsInfo_next, 'MergeKeys', true) 
+    else; 
+        ratsInfo_all = ratsInfo_next
+    end; 
+end; 
 
 
-%rename the CS scent columns so I know which includes the CS+ scent and
-%which includes the CS-
-ratsInfo.Properties.VariableNames(6) = "control_scent";
-ratsInfo.Properties.VariableNames(7) = "EtOH_scent";
-
-
-%Remove the columns that I don't need
-ratsInfo([41:end], :) = []
-
-%use function to add a new column with each rat's classification as an EtOH
-%drinker or EtOH nondrinker. Water drinkers are labeled as control 
-%[ratsInfo] = drinkingBehGroups(ratsInfo, IAPConsRaw, RAPConsRaw)
-
-%use function of add a new column with each rat's front-loading
-%classification 
-%[ratsInfo] = plotFrontLoadingHist(ratsInfo)
