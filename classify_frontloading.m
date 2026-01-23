71fc978038fd7b1826aa6df8118609b40e472be7
arremes <arremes417@gmail.com>
2026-01-23 10:49:32 -0500

    more frontloading classification using piecewise linear function

--- classify_frontloading.m
+++ classify_frontloading.m
@@ -16,6 +16,16 @@ for i = 1:numel(days)
         try 
             [nm, vals, fitResult, gof, pv] = getPwlSlopesDD(data{days(i)}(j,:)');
             res(j,:) = [vals(1) vals(2) vals(3) pv]; %[slope 1, slope 2, knot, pval]
+            % figure(1)
+            % plot(data{days(i)}(j,:),'ko-'); hold on
+            % plot(fitResult,'r-');
+            % xlabel('time(s)');
+            % ylabel('cumulative g/kg')
+            % titling = ['Day-' num2str(days(i)) '-rat-' num2str(ratsInfo.ratID(j))];
+            % title(titling);
+            % hold off
+            %  filename = fullfile('C:\Users\annar\OneDrive\Documents\IUSM\Dr. Lapish Lab\EtOH_scent_Urgency\graphs\frontloading\piecewiseLinearCode', [titling '.jpg']);
+            %  saveas(gcf, filename)
         catch 
             warning(['Day ' num2str(days(i)) ' row ' num2str(j) ' is not working'])
             res(j, :) = [0 0 0 0];
@@ -24,10 +34,8 @@ for i = 1:numel(days)
     all_results{i} = res;
 end 
 
+%% 
 Metabolic_Rate = 0.0001; %enter the metabolic rate in g/kg/your timescale (min or sec). 
 
 %check if the change point is in the first half of the session 
 
-%% Compare discounting to frontloading %%
-
-corrected_k = log(discount)
