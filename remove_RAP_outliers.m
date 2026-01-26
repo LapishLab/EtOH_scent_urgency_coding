c64995a8d70f5631d64f408a72e2f2f11c2a5b95
arremes <arremes417@gmail.com>
2026-01-25 20:06:24 -0500

    calculate and compare k to standardized RAP g/kg consumption and g/lick

--- remove_RAP_outliers.m
+++ remove_RAP_outliers.m
@@ -2,18 +2,18 @@
 %convert them to 0 in total licks and consumption 
 
 % rat subject IDs with an outlier 
-rats = [25 94 86 21 37 56]; 
+rats = [25 94 86 21 37 56 80 25 84]; 
 
 %days where the outlier is at. The day number aligns with the location of the rat
 %IDs in the rats vector
-days = [1 8 8 1 10 5];
+days = [1 8 8 1 10 5 2 4 7];
 
 %vector of the licks and consumption to make sure I am removing the correct
 %outlier. The consumption values are only the first 3 integers of the
 %consumption 
 
-licks = [2639 0 0 267 357 1436];
-cons = [0.08 1.1756 2.325 1.317 1.423 0.947];
+licks = [2639 0 0 267 357 1436 249 987 2];
+cons = [0.08 1.1756 2.325 1.317 1.423 0.947 10.384 33.566 1.980];
 
 for i = 1:numel(rats)
     %find the location of the outlier data in the matrix. Should match
@@ -25,9 +25,9 @@ for i = 1:numel(rats)
     %check that the location of the outlier data matches the day and rat
     %number that goes with that location
     if row == find(ratsInfo.ratID == rats(i)) & col == days(i)
-        RAP_totalLicks(row, col) = 0; RAP_all{row, col} = 0;
+        RAP_totalLicks(row, col) = NaN; RAP_all{row, col} = NaN;
     else 
-        warning("Location of outlier data isn't matching day and rat number")
+        warning(['Location of outlier data ' num2str(i) ' isnt matching day and rat number'])
     end 
 end 
 
--- variables/calc_consumption_lick.m
+++ variables/calc_consumption_lick.m
@@ -4,7 +4,7 @@ function group_cons_lick = calc_consumption_lick(RAP_all, RAP_totalLicks, opts)
 
 arguments 
     RAP_all %table that contains the total consumption for each rat each day 
-    RAP_totalLicks {mustBeInteger} = "" %matrix with total licks for each rat on each day
+    RAP_totalLicks %matrix with total licks for each rat on each day
     opts.days {mustBeVector} = [] %must be a x by 1 vector with the days you want to pull data from 
     opts.group (:,1) logical = [] %logical state of which rows/rats you want to analyze 
 end 
@@ -14,7 +14,7 @@ cons_lick = table2array(RAP_all) ./ RAP_totalLicks;
 
 %convert any infinity values to NaNs 
 %some values were divide by zero because lick measurer was faulty 
-cons_lick(isinf(cons_lick)) = NaN;
+cons_lick(isinf(cons_lick)) = 0;
 
 %initialize variables
 group_cons_lick = []
