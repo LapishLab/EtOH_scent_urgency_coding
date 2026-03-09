4d7b95160de599d42589e4052ce7783fa359b3dd
arremes <arremes417@gmail.com>
2026-03-09 16:40:46 -0400

    analyzing within subject comparisons in RAP pairings

--- RAP_pairingsCons.m
+++ RAP_pairingsCons.m
@@ -1,7 +1,7 @@
 
 %initialize variables
 %pull out the data for wistars (row 1) or P rats (row 2)
-sideMtx = RAP_sideMtx(2,[1 3 5 6 8 10]);
+sideMtx = RAP_sideMtx(1,[6 8 10]);
 
 %pull out ethanol and water animal subject numbers to compare to subject
 %numbers in the box to determine what each animals reinforcer was. 
@@ -16,11 +16,25 @@ data = [];
 %frontloading slopes (only have it for EtOH days in RAP)
 % data = slope1; 
 % total g/kg cons. Have to pull out only the EtOH days 
-data = table2array(RAP_all(:, [1 3 5 6 8 10]));
+data = RAP_all(:, [1 3 5 6 8 10]);
 % g/kg/lick. Have to pull out only the EtOH days
 % make any infinity values from not recording any licks set to 0 
 % data = table2array(RAP_all(:, [1 3 5 6 8 10])./RAP_totalLicks(:,[1 3 5 6 8 10]));
 % data(isinf(data)) = 0;
+
+%initialize variables
+EtOH_pairs = [];
+EtOH_pairs_subjects = [];
+water_pairs = [];
+water_pairs_subjects = [];
+mixPair_EtOH = [];
+mixPair_EtOH_subjects = [];
+mixPair_water = [];
+mixPair_water_subjects = [];
+lone_water = [];
+lone_water_subjects = [];
+lone_EtOH = [];
+lone_EtOH_subjects = [];
 
 %Cycle through each box pairing from RAP_sideMtx. Determine what the pairing of the 
 %subjects is (EtOH_EtOH, EtOH_water, by themselves water or etoh). Assign the subject
@@ -28,19 +42,6 @@ data = table2array(RAP_all(:, [1 3 5 6 8 10]));
 %and licks for each rat into a matrix specific to their pairing or lone
 %matrix for each day. 
 for i = 1:numel(sideMtx)
-     %initialize variables for each day
-     EtOH_pairs = [];
-     EtOH_pairs_subjects = [];
-     water_pairs = [];
-     water_pairs_subjects = [];
-     mixPair_EtOH = [];
-     mixPair_EtOH_subjects = [];
-     mixPair_water = [];
-     mixPair_water_subjects = [];
-     lone_water = [];
-     lone_water_subjects = [];
-     lone_EtOH = [];
-     lone_EtOH_subjects = [];
      for rat = 1:size(sideMtx{i}, 1)
         %pull out data for each day
         dayPairs = sideMtx{i};
@@ -93,31 +94,55 @@ for i = 1:numel(sideMtx)
             end
         end
      end
-     %add subjects for each day to an overall 'all' variable
-     %ethanol subject numbers
-     EtOH_pairs_subjects_all{i} = EtOH_pairs_subjects;
-     mixPair_EtOH_subjects_all{i} = mixPair_EtOH_subjects; 
-     lone_EtOH_subjects_all{i} = lone_EtOH_subjects;
-
-     %water subject numbers
-     water_pairs_subjects_all{i} = water_pairs_subjects;
-     mixPair_water_subjects_all{i} = mixPair_water_subjects;
-     lone_water_subjects_all{i} = lone_water_subjects;
-
-     %ethanol animal data
-     EtOH_pairs_all{i} = EtOH_pairs;
-     mixPair_EtOH_all{i} = mixPair_EtOH;
-     lone_EtOH_all{i} = lone_EtOH;
-     %water animal data
-     water_pairs_all{i} = water_pairs;
-     mixPair_water_all{i} = mixPair_water;
-     lone_water_all{i} = lone_water;
+     % %add subjects for each day to an overall 'all' variable
+     % %ethanol subject numbers
+     % EtOH_pairs_subjects_all{i} = EtOH_pairs_subjects;
+     % mixPair_EtOH_subjects_all{i} = mixPair_EtOH_subjects; 
+     % lone_EtOH_subjects_all{i} = lone_EtOH_subjects;
+     % 
+     % %water subject numbers
+     % water_pairs_subjects_all{i} = water_pairs_subjects;
+     % mixPair_water_subjects_all{i} = mixPair_water_subjects;
+     % lone_water_subjects_all{i} = lone_water_subjects;
+     % 
+     % %ethanol animal data
+     % EtOH_pairs_all{i} = EtOH_pairs;
+     % mixPair_EtOH_all{i} = mixPair_EtOH;
+     % lone_EtOH_all{i} = lone_EtOH;
+     % %water animal data
+     % water_pairs_all{i} = water_pairs;
+     % mixPair_water_all{i} = mixPair_water;
+     % lone_water_all{i} = lone_water;
 end
+
+%% Within-subject Comparisons Across Pairings %%
+
+%average the data within subjects for each EtOH-EtOH paired animal
+[samePair_avg_data, samePair_avg_subjects] = RAP_pairs_avg_subject(water_pairs, water_pairs_subjects);
+
+%average the data within subject for each EtOH-H2O paired animal 
+[mixPair_avg_data, mixPair_avg_subjects] = RAP_pairs_avg_subject(mixPair_water, mixPair_water_subjects);
+
+%pull out ratIDs of group that you want to look at 
+rats = ratsInfo.ratID(ratsInfo.treatment == "Control" & ratsInfo.strain == "Wistar" & ratsInfo.sex == "F") %& (ratsInfo.drinkClass == "High" | ratsInfo.drinkClass == "Medium"));
+
+within_pairs_data = []; 
+within_subjects = [];
+
+for i = 1:numel(rats)
+    loc1 = rats(i) == samePair_avg_subjects;
+    loc2 = rats(i) == mixPair_avg_subjects;
+    if any(loc1) & any(loc2)
+        within_subjects = [within_subjects; rats(i)] ;
+        within_pairs_data = [within_pairs_data; EtOH_avg_data(loc1) mixPair_EtOH_avg_data(loc2)];
+    end 
+end 
+
 
 %% Pull out grouped data %%
 
 %grouped ratIDs
-rats = ratsInfo.ratID(ratsInfo.treatment == "EtOH" & ratsInfo.strain == "P" & ratsInfo.sex == "M") %& (ratsInfo.drinkClass == "High" | ratsInfo.drinkClass == "Medium"));
+rats = ratsInfo.ratID(ratsInfo.treatment == "EtOH" & ratsInfo.strain == "Wistar" & ratsInfo.sex == "M") & (ratsInfo.drinkClass == "High" | ratsInfo.drinkClass == "Medium"));
 
 groupData = [];
 toAnalyze = EtOH_pairs_all;
---
+++ RAP_pairs_avg_subject.m
@@ -0,0 +1,24 @@
+function [avg_data, avg_subjects] = RAP_pairs_avg_subject(data, all_subjects);
+%% Average animal consumption if in RAP Pairing across Multiple Days %%
+% then can look at within subject pairings 
+%inputs:
+    % subjects: array of subject numbers that produced the data in the
+    % matching row in data
+    % data: some measured value from RAP (licks, g/kg cons, g/kg/lick)
+
+%remove repeated numbers in subjects and sort in increasing order
+summary_subjects = sort(unique(all_subjects));
+
+avg_data = [];
+avg_subjects = [];
+
+%take the mean of the data if a subject is found multiple times in
+%all_subjects
+for i = 1:numel(summary_subjects)
+    %location of all data for subjects
+    match = all_subjects == summary_subjects(i);
+    %calculate the mean of the data if there are multiple data points and
+    %add to matrix
+    avg_data = [avg_data; mean(data(match), 'omitnan')];
+    avg_subjects = [avg_subjects; summary_subjects(i)];
+end
